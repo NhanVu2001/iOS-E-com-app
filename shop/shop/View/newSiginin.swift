1b8bba1f6729f183161910dd6246e6202e038248
NhanVu2001 <vtnhan1590@gmail.com>
2025-01-06 17:15:30 +0700

    Update newSiginin.swift

--- shop/shop/View/newSiginin.swift
+++ shop/shop/View/newSiginin.swift
@@ -1,19 +1,3 @@
-/*
- RMIT University Vietnam
- Course: COSC2659 iOS Development
- Semester: 2022B
- Assessment: Assignment 3
- Author: Group 22
- Dao Minh Cuong: s3866133
- Nguyen Cong Phuong: s3804846
- Vu Thien Nhan: s3810151
- Tran Truong Son: s3818468
- Created  date: dd/mm/yyyy (e.g. 31/07/2022)
- Last modified: dd/mm/yyyy (e.g. 05/08/2022)
- Acknowledgement: https://www.youtube.com/watch?v=bRnBPJ_dzpg&list=PLimqJDzPI-H819xpEm_sc3kOcHLq9FtvF
- https://firebase.google.com/docs/firestore/solutions/geoqueries
- */
-
 import SwiftUI
 import Firebase
 let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
