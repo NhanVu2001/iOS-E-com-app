048e399fe9c8fa61d7e300416211437c9fe9a432
NhanVu2001 <vtnhan1590@gmail.com>
2025-01-06 17:15:41 +0700

    Update signUp.swift

--- shop/shop/View/signUp.swift
+++ shop/shop/View/signUp.swift
@@ -1,19 +1,3 @@
-/*
- RMIT University Vietnam
- Course: COSC2659 iOS Development
- Semester: 2022B
- Assessment: Assignment 3
- Author: Group 22
- Dao Minh Cuong: s3866133
- Nguyen Cong Phuong: s3804846
- Vu Thien Nhan: s3810151
- Tran Truong Son: s3818468
- Created  date: dd/mm/yyyy (e.g. 31/07/2022)
- Last modified: dd/mm/yyyy (e.g. 05/08/2022)
- Acknowledgement: https://www.youtube.com/watch?v=bRnBPJ_dzpg&list=PLimqJDzPI-H819xpEm_sc3kOcHLq9FtvF
- https://firebase.google.com/docs/firestore/solutions/geoqueries
- */
-
 import Firebase
 import SwiftUI
 import CoreLocation
